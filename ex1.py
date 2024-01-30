e3389baa1a251bc342f6f5eb42e612d5ee85bc54
iBobbyTS <iBobbyTS@gmail.com>
2024-01-30 15:56:13 -0700

    re-format and graph

--- ex1.py
+++ ex1.py
@@ -1,3 +1,7 @@
+import timeit
+import matplotlib.pyplot as plt
+
+
 # 1. The code calculates the fibononocci number in the nth position
 # 2. This is a divide-and-conquer alogorithm as it splits the problem by
 #    recursively calling func until n is 0 or 1 and merges each subproblem
@@ -9,17 +13,31 @@ def func(n):
     if n == 0 or n == 1:
         return n
     else:
-        return func(n-1) + func(n-2)
+        return func(n - 1) + func(n - 2)
 
-#4
+
+# 4
 def func_memo(n, memo={}):
     if n in memo:
         return memo[n]
     if n == 0 or n == 1:
         return n
     else:
-         memo[n] = func_memo(n-1, memo) + func_memo(n-2, memo)
-         return memo[n]
-    
-n = int(input())
-print(func_memo(n))
+        memo[n] = func_memo(n - 1, memo) + func_memo(n - 2, memo)
+        return memo[n]
+
+
+# n = int(input())
+# print(func_memo(n))
+exec_time = []
+for n in range(36):
+    original_time = timeit.timeit(f'func({n})', number=1, globals=globals())
+    improved_time = timeit.timeit(f'func_memo({n})', number=1, globals=globals())
+    exec_time.append([original_time, improved_time])
+    # print(f'Original time: {original_time}; Improved time: {improved_time}')
+plt.plot(range(36), [i[0] for i in exec_time], label='Original')
+plt.plot(range(36), [i[1] for i in exec_time], label='Improved')
+plt.xlabel('n')
+plt.ylabel('Execution time')
+plt.legend()
+plt.show()
